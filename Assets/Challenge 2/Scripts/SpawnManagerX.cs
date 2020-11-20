e8b24b0115d038c27685bebaaea8a55399181df1
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-11-20 16:42:12 -0500

    random balls

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
-    public GameObject[] ballPrefabs;
+	public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+	private float spawnLimitXLeft = -22;
+	private float spawnLimitXRight = 7;
+	private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+	private float startDelay = 1.0f;
+	private float spawnInterval = 4.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+	}
 
-    // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
-    {
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+	// Spawn random ball at random x position at top of play area
+	void SpawnRandomBall()
+	{
+		// Generate random ball index and random spawn position
+		Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
-    }
+		// instantiate ball at random spawn location
+		int chosen = Random.Range(0, ballPrefabs.Length);
+		Instantiate(ballPrefabs[chosen], spawnPos, ballPrefabs[chosen].transform.rotation);
+	}
 
 }
