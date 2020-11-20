f5685b879a2faa34db6ab223fa99e1ad9f018907
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-11-20 16:36:44 -0500

    better ball collisions

--- Assets/Challenge 2/Scripts/DetectCollisionsX.cs
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class DetectCollisionsX : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		Destroy(other.gameObject);
+	}
 }
