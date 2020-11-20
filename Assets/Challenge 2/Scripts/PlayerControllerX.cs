04fb8555104276b45817dd135af37f81332c8637
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-11-20 16:46:35 -0500

    anti spam

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,17 @@ public class PlayerControllerX : MonoBehaviour
 {
 	public GameObject dogPrefab;
 
+	public float timer = 0.0f;
+
 	// Update is called once per frame
 	void Update()
 	{
+		timer += Time.deltaTime;
 		// On spacebar press, send dog
-		if (Input.GetKeyDown(KeyCode.Space) || (Application.isEditor && Input.GetKeyDown(KeyCode.O)))
+		if (timer > 2 && (Input.GetKeyDown(KeyCode.Space) || (Application.isEditor && Input.GetKeyDown(KeyCode.O))))
 		{
 			Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+			timer = 0.0f;
 		}
 	}
 }
